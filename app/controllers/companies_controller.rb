abca11b8812c10f7915530a31ebb6c47af0f5cc7
Carlos Luna <cglunag@gmail.com>
2017-02-26 18:31:44 +0100

    added error messages on save new companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,13 +9,17 @@ class CompaniesController < ApplicationController
   end
 
   def new
+    @company = Company.new
   end
 
   def create
     @company = Company.new(company_params)
 
-    @company.save
+     if @company.save
     redirect_to @company
+     else
+       render 'new'
+     end
   end
 
   private
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+
+validates :name, :email, :city, presence: true
+
+
 end
