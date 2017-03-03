2dc91b526ff0ac7e51419d4eeaf223d7a28685d0
Carlos Luna <cglunag@gmail.com>
2017-03-03 12:36:42 +0100

    just commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -54,7 +54,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:title, :description, :price)
+    params.require(:product).permit(:title, :description, :image, :price)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ApplicationRecord
 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates :image, :attachment_content_type => { :content_type => ['image/png', 'image/jpg']}
+
+   attr_accessor :image_file_name
+
+
+
   has_many :comments, dependent: :destroy
 
   validates :title, :price, presence: true
